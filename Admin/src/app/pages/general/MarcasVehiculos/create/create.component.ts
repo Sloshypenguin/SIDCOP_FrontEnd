ed94dbc9f89a1c6d0907f9fc273313b57c99a770
leroyarguijo <77426786+leroyarguijo@users.noreply.github.com>
2025-07-14 15:52:47 -0600

    Marca De Vehiculos
    --CRUD completo(terminado)

--- Admin/src/app/pages/general/MarcasVehiculos/create/create.component.ts
+++ Admin/src/app/pages/general/MarcasVehiculos/create/create.component.ts
@@ -1,12 +1,145 @@
-import { Component } from '@angular/core';
+
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MarcasVehiculos } from 'src/app/Modelos/general/MarcasVehiculos.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+  @Output() onCancel = new EventEmitter<void>();
+  @Output() onSave = new EventEmitter<MarcasVehiculos>();
+  
+  mostrarErrores = false;
+  mostrarAlertaExito = false;
+  mensajeExito = '';
+  mostrarAlertaError = false;
+  mensajeError = '';
+  mostrarAlertaWarning = false;
+  mensajeWarning = '';
 
+  constructor(private http: HttpClient) {}
+
+  marca: MarcasVehiculos = {
+    maVe_Id: 0,
+    maVe_Marca: '',
+    usua_Creacion: 0,
+    usua_Modificacion: 0,
+    maVe_Estado: true,
+    maVe_FechaCreacion: new Date(),
+    maVe_FechaModificacion: new Date(),
+    code_Status: 0,
+    message_Status: '',
+    usuarioCreacion: '',
+    usuarioModificacion: ''
+  };
+
+  cancelar(): void {
+    this.mostrarErrores = false;
+    this.mostrarAlertaExito = false;
+    this.mensajeExito = '';
+    this.mostrarAlertaError = false;
+    this.mensajeError = '';
+    this.mostrarAlertaWarning = false;
+    this.mensajeWarning = '';
+    this.marca = {
+      maVe_Id: 0,
+      maVe_Marca: '',
+      usua_Creacion: 0,
+      usua_Modificacion: 0,
+      maVe_Estado: true,
+      maVe_FechaCreacion: new Date(),
+      maVe_FechaModificacion: new Date(),
+      code_Status: 0,
+      message_Status: '',
+      usuarioCreacion: '',
+      usuarioModificacion: ''
+    };
+    this.onCancel.emit();
+  }
+
+  cerrarAlerta(): void {
+    this.mostrarAlertaExito = false;
+    this.mensajeExito = '';
+    this.mostrarAlertaError = false;
+    this.mensajeError = '';
+    this.mostrarAlertaWarning = false;
+    this.mensajeWarning = '';
+  }
+
+  guardar(): void {
+    this.mostrarErrores = true;
+    
+    if (this.marca.maVe_Marca.trim()) {
+      // Limpiar alertas previas
+      this.mostrarAlertaWarning = false;
+      this.mostrarAlertaError = false;
+      
+      const marcaGuardar = {
+        maVe_Id: 0,
+        maVe_Marca: this.marca.maVe_Marca.trim(),
+        usua_Creacion: environment.usua_Id,// varibale global, obtiene el valor del environment, esto por mientras
+        maVe_FechaCreacion: new Date().toISOString(),
+        usua_Modificacion: 0,
+        maVe_FechaModificacion: new Date().toISOString(),
+        usuarioCreacion: "", 
+        usuarioModificacion: "" 
+      };
+
+      console.log('Guardando marca:', marcaGuardar);
+      
+      this.http.post<any>(`${environment.apiBaseUrl}/MarcasVehiculos/Insertar`, marcaGuardar, {
+        headers: { 
+          'X-Api-Key': environment.apiKey,
+          'Content-Type': 'application/json',
+          'accept': '*/*'
+        }
+      }).subscribe({
+        next: (response) => {
+          console.log('Marca guardada exitosamente:', response);
+          this.mensajeExito = `Marca "${this.marca.maVe_Marca}" guardada exitosamente`;
+          this.mostrarAlertaExito = true;
+          this.mostrarErrores = false;
+          
+          // Ocultar la alerta después de 3 segundos
+          setTimeout(() => {
+            this.mostrarAlertaExito = false;
+            this.onSave.emit(this.marca);
+            this.cancelar();
+          }, 3000);
+        },
+        error: (error) => {
+          console.error('Error al guardar marca:', error);
+          this.mostrarAlertaError = true;
+          this.mensajeError = 'Error al guardar la marca. Por favor, intente nuevamente.';
+          this.mostrarAlertaExito = false;
+          
+          // Ocultar la alerta de error después de 5 segundos
+          setTimeout(() => {
+            this.mostrarAlertaError = false;
+            this.mensajeError = '';
+          }, 5000);
+        }
+      });
+    } else {
+      // Mostrar alerta de warning para campos vacíos
+      this.mostrarAlertaWarning = true;
+      this.mensajeWarning = 'Por favor complete todos los campos requeridos antes de guardar.';
+      this.mostrarAlertaError = false;
+      this.mostrarAlertaExito = false;
+      
+      // Ocultar la alerta de warning después de 4 segundos
+      setTimeout(() => {
+        this.mostrarAlertaWarning = false;
+        this.mensajeWarning = '';
+      }, 4000);
+    }
+  }
 }
--- Admin/src/app/pages/general/MarcasVehiculos/edit/edit.component.ts
+++ Admin/src/app/pages/general/MarcasVehiculos/edit/edit.component.ts
@@ -1,12 +1,142 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MarcasVehiculos } from 'src/app/Modelos/general/MarcasVehiculos.model';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
-export class EditComponent {
+export class EditComponent implements OnChanges {
+  @Input() marcasVehiculosData: MarcasVehiculos | null = null;
+  @Output() onCancel = new EventEmitter<void>();
+  @Output() onSave = new EventEmitter<MarcasVehiculos>();
 
+ marcasVehiculos: MarcasVehiculos = {
+    maVe_Id: 0,
+    maVe_Marca: '',
+    usua_Creacion: 0,
+    usua_Modificacion: 0,
+    maVe_Estado: true,
+    maVe_FechaCreacion: new Date(),
+    maVe_FechaModificacion: new Date(),
+    code_Status: 0,
+    message_Status: '',
+    usuarioCreacion: '',
+    usuarioModificacion: ''
+  };
+
+  marcasVehiculosOriginal = '';
+  mostrarErrores = false;
+  mostrarAlertaExito = false;
+  mensajeExito = '';
+  mostrarAlertaError = false;
+  mensajeError = '';
+  mostrarAlertaWarning = false;
+  mensajeWarning = '';
+  mostrarConfirmacionEditar = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['marcasVehiculosData'] && changes['marcasVehiculosData'].currentValue) {
+      this.marcasVehiculos = { ...changes['marcasVehiculosData'].currentValue };
+      this.marcasVehiculosOriginal = this.marcasVehiculos.maVe_Marca || '';
+      this.mostrarErrores = false;
+      this.cerrarAlerta();
+    }
+  }
+
+  cancelar(): void {
+    this.cerrarAlerta();
+    this.onCancel.emit();
+  }
+
+  cerrarAlerta(): void {
+    this.mostrarAlertaExito = false;
+    this.mensajeExito = '';
+    this.mostrarAlertaError = false;
+    this.mensajeError = '';
+    this.mostrarAlertaWarning = false;
+    this.mensajeWarning = '';
+  }
+
+  validarEdicion(): void {
+    this.mostrarErrores = true;
+
+    if (this.marcasVehiculos.maVe_Marca.trim()) {
+      if (this.marcasVehiculos.maVe_Marca.trim() !== this.marcasVehiculosOriginal) {
+        this.mostrarConfirmacionEditar = true;
+      } else {
+        this.mostrarAlertaWarning = true;
+        this.mensajeWarning = 'No se han detectado cambios.';
+        setTimeout(() => this.cerrarAlerta(), 4000);
+      }
+    } else {
+      this.mostrarAlertaWarning = true;
+      this.mensajeWarning = 'Por favor complete todos los campos requeridos antes de guardar.';
+      setTimeout(() => this.cerrarAlerta(), 4000);
+    }
+  }
+
+  cancelarEdicion(): void {
+    this.mostrarConfirmacionEditar = false;
+  }
+
+  confirmarEdicion(): void {
+    this.mostrarConfirmacionEditar = false;
+    this.guardar();
+  }
+
+  private guardar(): void {
+    this.mostrarErrores = true;
+
+    if (this.marcasVehiculos.maVe_Marca.trim()) {
+      const marcasVehiculosActualizar = {
+        maVe_Id: this.marcasVehiculos.maVe_Id,
+        maVe_Marca: this.marcasVehiculos.maVe_Marca.trim(),
+        usua_Creacion: this.marcasVehiculos.usua_Creacion,
+        maVe_FechaCreacion: this.marcasVehiculos.maVe_FechaCreacion,
+        usua_Modificacion: environment.usua_Id,
+        maVe_FechaModificacion: new Date().toISOString(),
+        usuarioCreacion: '',
+        usuarioModificacion: ''
+      };
+
+      this.http.put<any>(`${environment.apiBaseUrl}/MarcasVehiculos/Editar`, marcasVehiculosActualizar, {
+        headers: {
+          'X-Api-Key': environment.apiKey,
+          'Content-Type': 'application/json',
+          'accept': '*/*'
+        }
+      }).subscribe({
+        next: (response) => {
+          this.mensajeExito = `Marca "${this.marcasVehiculos.maVe_Marca}" actualizada exitosamente`;
+          this.mostrarAlertaExito = true;
+          this.mostrarErrores = false;
+
+          setTimeout(() => {
+            this.mostrarAlertaExito = false;
+            this.onSave.emit(this.marcasVehiculos);
+            this.cancelar();
+          }, 3000);
+        },
+        error: (error) => {
+          console.error('Error al actualizar marca:', error);
+          this.mostrarAlertaError = true;
+          this.mensajeError = 'Error al actualizar la marca. Por favor, intente nuevamente.';
+          setTimeout(() => this.cerrarAlerta(), 5000);
+        }
+      });
+    } else {
+      this.mostrarAlertaWarning = true;
+      this.mensajeWarning = 'Por favor complete todos los campos requeridos antes de guardar.';
+      setTimeout(() => this.cerrarAlerta(), 4000);
+    }
+  }
 }
