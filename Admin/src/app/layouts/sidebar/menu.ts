80f8e2ae2e376b25332a449831ada68d1589a257
Sloshypenguin <alejandrogaleas.64@gmail.com>
2025-07-15 13:44:48 -0600

    update (id rutas!)

--- Admin/src/app/layouts/sidebar/menu.ts
+++ Admin/src/app/layouts/sidebar/menu.ts
@@ -2,189 +2,189 @@ import { MenuItem } from "./menu.model";
 
 export const MENU: MenuItem[] = [
     {
-        id: 1,
+        id: 0,
         label: 'MENUITEMS.MENU.TEXT',
         isTitle: true
     },
     {
-        id: 2,
+        id: 1,
         label: 'MENUITEMS.DASHBOARD.TEXT',
         icon: 'ph-gauge',
         subItems: [
             {
-                id: 5,
+                id: 1,
                 label: 'MENUITEMS.DASHBOARD.LIST.ECOMMERCE',
                 link: '/',
-                parentId: 2
+                parentId: 1
             },
         ]
     },
     {
-        id: 3,
+        id: 2,
         label: 'MENUITEMS.GENERAL.TEXT',
         icon: 'ri-apps-2-line',
         subItems: [
             {
-                id: 31,
+                id: 14,
                 label: 'MENUITEMS.GENERAL.LIST.ESTADOSCIVILES',
                 link: '/general/estadosciviles',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-group-line'
             },
              {
-                id: 32,
+                id: 17,
                 label: 'MENUITEMS.GENERAL.LIST.MODELOS',
                 link: '/general/modelos',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-group-line'
             },
             {
-                id: 33,
+                id: 20,
                 label: 'MENUITEMS.GENERAL.LIST.SUCURSALES',
                 link: '/general/sucursales',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-group-line'
             },
             {
-                id: 34,
+                id: 15,
                 label: 'MENUITEMS.GENERAL.LIST.MARCAS',
                 link: '/general/marcas',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-group-line'
             },
             {
-                id: 35,
+                id: 12,
                 label: 'MENUITEMS.GENERAL.LIST.DEPARTAMENTOS',
                 link: '/general/departamentos',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-group-line'
             },
             {
-                id: 36,
+                id: 16,
                 label: 'MENUITEMS.GENERAL.LIST.MARCASVEHICULOS',
                 link: '/general/marcasvehiculos',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-group-line'
             },
             {
-                id: 37,
+                id: 18,
                 label: 'MENUITEMS.GENERAL.LIST.MUNICIPIOS',
                 link: '/general/municipios',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-group-line'
             },
             {
-                id: 38,
+                id: 11,
                 label: 'MENUITEMS.GENERAL.LIST.COLONIAS',
                 link: '/general/colonias', 
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-building-2-line'
             },    
             {
-                id: 39,
+                id: 8,
                 label: 'MENUITEMS.GENERAL.LIST.CANALES',
                 link: '/general/canales',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-building-2-line'
             },
 
             {
-                id: 38,
+                id: 19,
                 label: 'MENUITEMS.GENERAL.LIST.PROVEEDORES',
                 link: '/general/proveedores',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-group-line'
             },
  
             {
-                id: 35,
+                id: 13,
                 label: 'MENUITEMS.GENERAL.LIST.EMPLEADOS',
                 link: '/general/empleados',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-building-2-line'
             },
             {
-                id: 37,
+                id: 9,
                 label: 'MENUITEMS.GENERAL.LIST.CARGOS',
                 link: '/general/cargos',
-                parentId: 3,
+                parentId: 2,
                 icon: 'ri-building-2-line'
             },
 
         ]
     },
     {
-        id: 4,
+        id: 5,
         label: 'MENUITEMS.VENTAS.TEXT',
         icon: 'ri-shopping-cart-2-line',
         subItems: [
 
             {
-                id: 33,
+                id: 37,
                 label: 'MENUITEMS.VENTAS.LIST.IMPUESTOS',
                 link: '/ventas/impuestos',
-                parentId: 4,
+                parentId: 5,
                 icon: 'ri-group-line'
             },
             
             {
-                id: 34,
+                id: 33,
                 label: 'MENUITEMS.VENTAS.LIST.CONFIGURACIONFACTURA',
                 link: '/ventas/configuracion-factura',
-                parentId: 4,
+                parentId: 5,
                 icon: 'ri-group-line'
             },
 
             {
-                id: 35,
+                id: 32,
                 label: 'MENUITEMS.VENTAS.LIST.CAIS',
                 link: '/ventas/CAIs',
-                parentId: 4,
+                parentId: 5,
                 icon: 'ri-group-line'
             },
 
             {
-                id: 36,
+                id: 42,
                 label: 'MENUITEMS.VENTAS.LIST.VENDEDORES',
                 link: '/ventas/Vendedores',
-                parentId: 4,
+                parentId: 5,
                 icon: 'ri-group-line'
             },
             // Puedes añadir más subitems aquí según sea necesario
         ]
     },
     {
-        id: 5,
+        id: 4,
         label: 'MENUITEMS.LOGISTICA.TEXT',
         icon: 'ri-apps-2-line',
         subItems: [
             {
-                id: 34,
+                id: 28,
                 label: 'MENUITEMS.LOGISTICA.LIST.BODEGAS',
                 link: '/logistica/bodegas',
-                parentId: 5,
+                parentId: 4,
                 icon: 'ri-truck-line'
             }
             // Puedes añadir más subitems aquí según sea necesario
         ]
     },
     {
-        id: 6,
+        id: 3,
         label: 'MENUITEMS.INVENTARIO.TEXT',
         icon: 'ri-layout-grid-fill',
         subItems: [
             {
-                id: 40,
+                id: 21,
                 label: 'MENUITEMS.INVENTARIO.LIST.CATEGORIAS',
                 link: '/inventario/categorias',
-                parentId: 6,
+                parentId: 3,
                 icon: 'ri-truck-line'
             },
             {
-                id: 41,
+                id: 27,
                 label: 'MENUITEMS.INVENTARIO.LIST.SUBCATEGORIAS',
                 link: '/inventario/subcategorias',
-                parentId: 6,
+                parentId: 3,
                 icon: 'ri-truck-line'
             }
             // Puedes añadir más subitems aquí según sea necesario
