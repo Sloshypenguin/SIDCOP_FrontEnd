f005d0fae3d1d53f7cce24528fa8845d0f758715
Josidkk <Deybyjosuederascardenas@gmail.com>
2025-07-15 04:17:43 -0600

    listar Proveedor

---
+++ Admin/src/app/Modelos/general/Proveedor.Model.ts
@@ -0,0 +1,25 @@
+export class Proveedor{
+  prov_Id: number = 0;
+  prov_Codigo: string = '';
+  prov_NombreEmpresa: string = '';
+  colo_Id: number = 0;
+  prov_DireccionExacta: string = '';
+  prov_Telefono: string = '';
+  prov_Correo: string = '';
+  prov_Observaciones: string = '';
+
+  usua_Creacion: number = 0;
+  prov_FechaCreacion: Date = new Date();
+  usua_Modificacion?: number;
+
+  prov_FechaModificacion?: Date;
+  usuarioCreacion: string = '';
+  usuarioModificacion: string = '';
+
+  code_Status: number = 0;
+  message_Status: string ='';
+
+  constructor(init?: Partial<Proveedor>) {
+    Object.assign(this, init);
+  }
+}
