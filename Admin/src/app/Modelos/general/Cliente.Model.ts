9cab25e8e45d8e90c08089b5452ea30eda402c84
Helen Euceda <neuceda13@gmail.com>
2025-07-24 02:50:58 -0600

    Cliente Model

--- Admin/src/app/Modelos/general/Cliente.Model.ts
+++ Admin/src/app/Modelos/general/Cliente.Model.ts
@@ -2,21 +2,24 @@ export class Cliente{
 
     clie_Id: number = 0;
     clie_Codigo: string = '';
+    clie_Nacionalidad: string = '';
     clie_DNI: string = '';
     clie_RTN: string = '';
     clie_Nombres: string = '';
     clie_Apellidos: string = '';
-    nombreCompleto: string = '';
+    // nombreCompleto: string = '';
     clie_NombreNegocio: string = '';
     clie_ImagenDelNegocio: string = '';
     clie_Telefono: string = '';
     clie_Correo: string = '';
     clie_Sexo: string = '';
     clie_FechaNacimiento?: Date;
+    tiVi_Id: number = 0;
+    tiVi_Descripcion: string = '';
     cana_Id: number = 0;
     cana_Descripcion: string = '';
-    colo_Id: number = 0;
-    colo_Descripcion: string = '';
+    // colo_Id: number = 0;
+    // colo_Descripcion: string = '';
     esCv_Id: number = 0;
     esCv_Descripcion: string = '';
     ruta_Id: number = 0;
