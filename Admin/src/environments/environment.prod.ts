b43992f1841d49bbba27892f612cc85717eb05e3
AlexanderDeRivia03 <balkarut@gmail.com>
2025-07-25 01:19:57 -0600

    update
    update

--- Admin/src/environments/environment.prod.ts
+++ Admin/src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ export const environment = {
     appId: '',
     measurementId: ''
   },
-  apiBaseUrl: 'http://192.168.1.146:8091', // URL de producción para SIDCOP
+  apiBaseUrl: 'https://localhost:7071', // URL de producción para SIDCOP
   apiKey: 'bdccf3f3-d486-4e1e-ab44-74081aefcdbc',
   usua_Id: idUsuario // Se establecerá dinámicamente al iniciar sesión
 };
