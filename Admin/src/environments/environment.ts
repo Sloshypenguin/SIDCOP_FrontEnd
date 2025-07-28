ee3b9a8cf0f07f59cb226cd30661f34eddb7a0db
SOLANGRI69 <ajsl69777@gmail.com>
2025-07-28 09:06:26 -0600

    Update environment.ts

--- Admin/src/environments/environment.ts
+++ Admin/src/environments/environment.ts
@@ -18,7 +18,7 @@ export const environment = {
     appId: "1:916438010670:web:c70cf404da6c0fe7b048bf",
     measurementId: "G-1N6FB2GG55"
   },
-  apiBaseUrl: 'http://SIDCOP.somee.com', //cambiar si es necesario
+  apiBaseUrl: '192.168.1.146\PRACTICAGEN8', //cambiar si es necesario
   apiKey: 'bdccf3f3-d486-4e1e-ab44-74081aefcdbc',
   usua_Id: idUsuario, 
 };
