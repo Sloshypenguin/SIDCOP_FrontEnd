dfc91303eb54c86a5b8541cc3fd26d4b1cc11210
Stephtor29 <gisselletoro22@gmail.com>
2025-07-30 10:02:22 -0600

    Changes

--- Admin/src/app/layouts/sidebar/menu.ts
+++ Admin/src/app/layouts/sidebar/menu.ts
@@ -234,7 +234,7 @@ export const MENU: MenuItem[] = [
       },
 
       {
-        id: 29,
+        id: 31,
         label: 'MENUITEMS.LOGISTICA.LIST.TRASLADOS',
         link: '/logistica/traslados',
         parentId: 4,
