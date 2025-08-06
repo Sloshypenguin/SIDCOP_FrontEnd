376416214bc11a8254f758f94045ec908fe3d27d
SOLANGRI69 <ajsl69777@gmail.com>
2025-08-06 11:17:15 -0600

    06/08/2025 Arreglo Envoritemenr

--- Admin/src/environments/environment.prod.ts
+++ Admin/src/environments/environment.prod.ts
@@ -14,9 +14,9 @@ export const environment = {
     appId: "1:916438010670:web:c70cf404da6c0fe7b048bf",
     measurementId: "G-1N6FB2GG55"
   },
-   apiBaseUrl: 'https://localhost:7071', //cambiar si es necesario
+  // apiBaseUrl: 'https://localhost:7071', //cambiar si es necesario
   // apiBaseUrl: 'http://200.59.27.115:8091', //cambiar si es necesario
-  // apiBaseUrl: 'http://192.168.1.146:8091', //cambiar si es necesario
+   apiBaseUrl: 'http://192.168.1.146:8091', //cambiar si es necesario
   // apiBaseUrl: 'http://sidcop.somee.com', //cambiar si es necesario
 
   apiKey: 'bdccf3f3-d486-4e1e-ab44-74081aefcdbc',
