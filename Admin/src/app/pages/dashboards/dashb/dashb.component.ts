abb06b927cbef65cf306c6e14d90fdecb09fd65f
Stephy <gisselletoro22@gmail.com>
2025-08-06 16:13:50 -0600

    FIX(Dash)

--- Admin/src/app/pages/dashboards/dashb/dashb.component.ts
+++ Admin/src/app/pages/dashboards/dashb/dashb.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
+import { BreadcrumbsComponent } from 'src/app/shared/breadcrumbs/breadcrumbs.component';
 import { environment } from '../../../../environments/environment.prod';
 import { getUserId, getUserName, getUserRole } from '../../../core/utils/user-utils';
 
@@ -12,13 +13,14 @@ import { Store } from '@ngrx/store';
 
 import { NgApexchartsModule } from 'ng-apexcharts';
 import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, NgZone } from '@angular/core';
 
 
 
 @Component({
   selector: 'app-dashb',
   standalone: true,
-  imports: [NgApexchartsModule],
+  imports: [CommonModule, NgApexchartsModule, FormsModule, BreadcrumbsComponent],
   templateUrl: './dashb.component.html',
   styleUrl: './dashb.component.scss'
 })
@@ -28,6 +30,13 @@ export class DashbComponent implements OnInit {
   http = inject(HttpClient);
   chart: any;
   ventasPorMesData: any[] = [];
+  aniosSelect: number[] = [];
+  anioSeleccionado: number = 2025;
+  barraMesSelected: any;
+
+  graficocategorias: boolean = false;
+  categoriasdata: any[] = [];
+  simpleDonutChart: any;
 
   totalrevenueChart: any;
 
@@ -44,12 +53,16 @@ export class DashbComponent implements OnInit {
   isProd: boolean = false;
   entorno: string = '';
 
-  constructor() { }
+  constructor(private cdr: ChangeDetectorRef,private ngZone: NgZone) 
+  { 
+  }
 
   ngOnInit(): void {
     
     this.cargarDatosSesion();
     this.cargardatos();
+
+    this._simpleDonutChart('["--tb-primary", "--tb-success", "--tb-warning", "--tb-danger", "--tb-info"]');
 
     // this._totalrevenueChart('["--table table-gridjs"]');
     // this.chart = new ApexCharts(document.querySelector(".apex-charts"), this.totalrevenueChart);
@@ -81,24 +94,55 @@ export class DashbComponent implements OnInit {
 
   private _totalrevenueChart(colors: any) {
     colors = this.getChartColorsArray(colors);
+    
+  const dataForYear = this.ventasPorMesData.filter(item => item.Anio == this.anioSeleccionado);
+  
+  dataForYear.sort((a, b) => a.MesNumero - b.MesNumero);
+
+  
+
     this.totalrevenueChart = {
       series: [{
-        name: 'Ventas',
-        data: this.ventasPorMesData.map(item => item.Cantidad || 0) 
+        name: 'Cantidad de Ventas',
+        data: dataForYear.map(item => item.Cantidad || 0)
+        // this.ventasPorMesData.filter(item => item.Anio == this.anioSeleccionado ).map(item => item.Cantidad || 0) 
         // data: [26, 24.65, 18.24, 29.02, 23.65, 27, 21.18, 24.65, 27.32, 25, 24.65, 29.32]
 
       }],
       chart: {
+        
         type: 'bar',
         height: 328,
         stacked: true,
         toolbar: {
-          show: true
+          show: true,
+          export: {
+            csv: {
+            headerCategory: 'Mes',
+            }
+          },
+
         },
+        events: {
+          dataPointSelection: (event: any, chartContext: any, config: any) => {
+            // info about the clicked bar
+            const seriesIndex = config.seriesIndex;
+            const dataPointIndex = config.dataPointIndex;
+            const value = config.w.globals.initialSeries[seriesIndex].data[dataPointIndex];
+            // const category = config.w.globals.categoryLabels[dataPointIndex];
+
+            this.barraMesSelected = dataForYear[dataPointIndex]; // or any other property
+
+            const category = config.w.globals.labels[dataPointIndex];
+            
+            this.onBarClick(category, value);
+          }
+        }
+        
       },
       plotOptions: {
         bar: {
-          columnWidth: '30%',
+          columnWidth: '80%',
           borderRadius: 5
         },
       },
@@ -106,7 +150,7 @@ export class DashbComponent implements OnInit {
         padding: {
           left: 0,
           right: 0,
-          top: -15,
+          top: 0,
           bottom: 0
         }
       },
@@ -119,6 +163,12 @@ export class DashbComponent implements OnInit {
         textAnchor: 'top',
       },
       yaxis: {
+        title: {
+          text: 'Cantidad de Ventas',
+          style: {
+            fontWeight: 600
+          }
+        },
         labels: {
           show: true,
           formatter: function (y: any) {
@@ -132,7 +182,14 @@ export class DashbComponent implements OnInit {
         horizontalAlign: 'right',
       },
       xaxis: {
-        categories: this.ventasPorMesData.map(item => item.MesNombre),
+        title: {
+          text: 'Meses',
+          style: {
+            fontWeight: 600
+          }
+        },
+        categories: dataForYear.map(item => item.MesNombre),
+        // this.ventasPorMesData.filter(item => item.Anio === this.anioSeleccionado).map(item => item.MesNombre ),
         // categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         labels: {
           rotate: -90
@@ -143,7 +200,7 @@ export class DashbComponent implements OnInit {
         axisBorder: {
           show: true,
           stroke: {
-            width: 1
+            width: 10
           },
         },
       }
@@ -159,6 +216,49 @@ export class DashbComponent implements OnInit {
     // });
   }
 
+  private _simpleDonutChart(colors: any) {
+    colors = this.getChartColorsArray(colors);
+    this.simpleDonutChart = {
+      series: this.categoriasdata.map(item => item.Cantidad),
+      
+      labels: this.categoriasdata.map(item => item.Categoria) ,
+      chart: {
+        height: 300,
+        type: "donut",
+      },
+      legend: {
+        position: "bottom",
+      },
+      dataLabels: {
+        dropShadow: {
+          enabled: false,
+        },
+      },
+      colors: colors,
+      tooltip: {
+      y: {
+        formatter: (val: number, opts: any) => {
+          // opts contains info about the hovered slice
+          const label = opts.w.globals.labels[opts.seriesIndex];
+          // You can access more data from this.categoriasdata if needed
+          return `${label}: ${val} compras`;
+        }
+      }
+    }
+    };
+
+
+    const attributeToMonitor = 'data-theme';
+
+    const observer = new MutationObserver(() => {
+      this._simpleDonutChart('["--tb-primary", "--tb-success", "--tb-warning", "--tb-danger", "--tb-info"]');
+    });
+    observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: [attributeToMonitor]
+    });
+  }
+
   private cargardatos(): void {
       // this.mostrarOverlayCarga = true;
       this.http.get(`${environment.apiBaseUrl}/Dashboards/VentasPorMes`, {
@@ -167,10 +267,19 @@ export class DashbComponent implements OnInit {
         next: data => {
 
           console.log('Datos de ventas por mes:', data);
+          
+          // console.log('añong', new Date().getFullYear());
           this.ventasPorMesData = data as any[];
+
+          
+          this.anioSeleccionado = this.ventasPorMesData.sort((a, b) => b.Anio - a.Anio)[0]?.Anio;
+          this.aniosSelect = this.ventasPorMesData.map(item => item.Anio)
+                                  .filter((anio, index, self) => self.indexOf(anio) === index);
+          console.log('añong22', this.anioSeleccionado);
+          
           this._totalrevenueChart('["--table table-gridjs"]');
 
-          this.chart = new ApexCharts(document.querySelector(".apex-charts"), this.totalrevenueChart);
+          // this.chart = new ApexCharts(document.querySelector(".apex-charts"), this.totalrevenueChart);
           // this.chart.render();          
 
 
@@ -182,6 +291,35 @@ export class DashbComponent implements OnInit {
         },
         error: error => {
           console.error('Error al cargar roles:', error);
+          
+        }
+      });
+    }
+
+    private cargardatosCategorias(): void {
+      // this.mostrarOverlayCarga = true;
+      const apibody: any = {
+        mes: this.barraMesSelected.Mes,
+        anio: this.barraMesSelected.Anio,
+        cate_Id: 0
+      };
+
+      this.http.post(`${environment.apiBaseUrl}/Dashboards/VentasPorMesCategorias`, 
+        apibody,
+        {
+        headers: { 'x-api-key': environment.apiKey }
+      }).subscribe({
+        next: data => {
+
+          console.log('Datos de ventas por mes:', data);
+          this.categoriasdata = data as any[];
+          
+          this._simpleDonutChart('["--tb-primary", "--tb-success", "--tb-warning", "--tb-danger", "--tb-info"]');
+
+          
+        },
+        error: error => {
+          console.error('Error al cargar categorias:', error);
           
         }
       });
@@ -216,6 +354,31 @@ export class DashbComponent implements OnInit {
     this.isProd = isProduction;
     this.entorno = isProduction ? 'Producción' : 'Desarrollo';
   }
+
+  cambioAnio(): void {
+    
+    this._totalrevenueChart('["--table table-gridjs"]');
+    // this.chart = new ApexCharts(document.querySelector(".apex-charts"), this.totalrevenueChart);
+    // this.chart.updateptions(this.totalrevenueChart);
+
+    console.log('ventas meses luego', this.ventasPorMesData);
+    
+  }
+
+  onBarClick(category: string, value: any) {
+  // Do something with the clicked bar info
+    console.log('Clicked bar:', category, value);
+    console.log('Barra seleccionada:', this.barraMesSelected);
+    // this.graficocategorias = true;
+
+    this.ngZone.run(() => {
+      this.cargardatosCategorias();
+      this.graficocategorias = true;
+      this.cdr.detectChanges();
+    });
+    
+    
+}
 
 }
 
